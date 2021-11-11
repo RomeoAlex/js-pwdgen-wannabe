c4a9c562ee2c7962cdf65c53f3d34ab4bb6dc3a7
RomeoAlex <romeo.alessandro17@gmail.com>
2021-11-11 16:29:35 +0100

    esercizio finito

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 // chiediamo all'utente il nome
 const userName = prompt('Come ti chiami?');
 console.log(userName);
+const userLastName = prompt('Quale è il tuo cognome?');
+console.log(userLastName);
+const userColor = prompt('Quale è il tuo colore preferito?');
+console.log(userColor);
 //Creo il messaggio di saluto
 // const userMessage = 'Ciao' + ' ' + userName;
-const userMessage = `Ciao ${userName}`;
+const userMessage = `La password generata è: ${userName}${userLastName}${userColor}21`;
 console.log(userMessage);
 //stampiamo a video
 document.getElementById('user-message').innerHTML = userMessage;

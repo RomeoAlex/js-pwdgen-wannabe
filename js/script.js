a432aa31fe7e4db857e1f8f8f2f736d15994f1ce
RomeoAlex <romeo.alessandro17@gmail.com>
2021-11-11 16:55:41 +0100

    corretto metodo senza backtick

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ const userLastName = prompt('Quale è il tuo cognome?');
 console.log(userLastName);
 const userColor = prompt('Quale è il tuo colore preferito?');
 console.log(userColor);
-//Creo il messaggio di saluto
-// const userMessage = 'Ciao' + ' ' + userName;
-const userMessage = `La password generata è: ${userName}${userLastName}${userColor}21`;
+//Creo il messaggio con la password
+const userMessage = 'La password generata è:' + ' ' + userName + userLastName + userColor +'21';
+// const userMessage = `La password generata è: ${userName}${userLastName}${userColor}21`;
 console.log(userMessage);
 //stampiamo a video
 document.getElementById('user-message').innerHTML = userMessage;

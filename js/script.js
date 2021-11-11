9f9242bd5cda6280d3d68fa8bc9c577c87916f9e
RomeoAlex <romeo.alessandro17@gmail.com>
2021-11-11 16:20:55 +0100

    test principale svolto

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
-// test
-alert('Ciao');
+// chiediamo all'utente il nome
+const userName = prompt('Come ti chiami?');
+console.log(userName);
+//Creo il messaggio di saluto
+// const userMessage = 'Ciao' + ' ' + userName;
+const userMessage = `Ciao ${userName}`;
+console.log(userMessage);
+//stampiamo a video
+document.getElementById('user-message').innerHTML = userMessage;
+

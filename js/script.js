dec99112499f0c5282156f7fea7669cbf22d5d00
RomeoAlex <romeo.alessandro17@gmail.com>
2021-11-12 15:29:57 +0100

    ultime correzioni

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 // chiediamo all'utente il nome
 const userName = prompt('Come ti chiami?');
+// il console.log serve solo per il debug
 console.log(userName);
 const userLastName = prompt('Quale è il tuo cognome?');
 console.log(userLastName);
 const userColor = prompt('Quale è il tuo colore preferito?');
 console.log(userColor);
 //Creo il messaggio con la password
-const userMessage = 'La password generata è:' + ' ' + userName + userLastName + userColor +'21';
-// const userMessage = `La password generata è: ${userName}${userLastName}${userColor}21`;
-console.log(userMessage);
+// const userPassword = 'La password generata è:' + ' ' + userName + userLastName + userColor +'21';
+// const userPassword = `La password generata è: ${userName}${userLastName}${userColor}21`;
+
+// Utilizzo somma per creare la password
+
+const userPassword = userName + userLastName + userColor + 21;
+console.log(userPassword);
+
 //stampiamo a video
-document.getElementById('user-message').innerHTML = userMessage;
+document.getElementById('user-message').innerHTML = userPassword;
 
